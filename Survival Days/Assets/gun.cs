e26b6c9bea0d73b98ab2a86171edca421d246666
IIS76II <108532889+IIS76II@users.noreply.github.com>
2022-09-07 14:14:55 +0300

    day 2

--- Survival Days/Assets/gun.cs
+++ Survival Days/Assets/gun.cs
@@ -4,15 +4,20 @@ public class gun : MonoBehaviour
 {
     public float damage = 20f;
     public float range = 100f;
+    public float firerate = 15f;
     public Camera fpscam;
     public ParticleSystem muzzle;
+    public GameObject impacteffect;
+
+    private float nextTimetofire = 0f;
 
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= nextTimetofire)
         {
+            nextTimetofire = Time.time + 1 / firerate;
             shoot();
         }
     }
@@ -31,6 +36,8 @@ public class gun : MonoBehaviour
             {
                 target.TakeDamage(damage);
             }
+           GameObject impactGO = Instantiate(impacteffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 1f);
         }
     }
 }
